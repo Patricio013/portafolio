4d06c591e45dfe2c0857430cb64b73099ba2df50
Patricio013 <146967055+Patricio013@users.noreply.github.com>
2025-01-21 12:07:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import Inicio from './pantallas/Inicio';
 import Proyectos from './pantallas/Proyectos';
 import Detalles from './pantallas/Detalles';
 import SobreMi from './pantallas/SobreMi';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename='/portafolio'>
       <Routes>
         <Route path='/' element={<Inicio />} />
         <Route path='/sobremi' element={<SobreMi />} />
         <Route path='/proyectos' element={<Proyectos />} />
         <Route path='/detalles' element={<Detalles />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
